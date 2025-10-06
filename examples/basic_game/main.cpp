df93eef79996841aad4b1b3c55c278b413c4863f
trapperdot00 <csernusricsike@gmail.com>
2025-10-06 14:26:34 +0200

    Modified Makefile inside the example to include the directories of included headers to make the #include directives easier to read. Added comments to example.

--- examples/basic_game/main.cpp
+++ examples/basic_game/main.cpp
@@ -1,7 +1,22 @@
-#include "../../src/game/cli_game.h"
-#include "../../src/tile/colored_numbered_tile.h"
+#include "cli_game.h"
+#include "colored_numbered_tile.h"
 
+#include <stdexcept>
 #include <string>
+
+/*	Example usage of cli_game
+ *
+ *	This program constructs a cli_game object with the cube length
+ *	passed in as a command-line argument.
+ *	
+ *	The tile objects are of type colored_numbered_tile, each tile
+ *	shows up as a color (abbreviated as a single character),
+ *	and a number unique to that color.
+ *	
+ *	The game is started using the play method of cli_game,
+ *	and takes in moves from the console using the axis-offset notation.
+ *	Moves are asked from the user until the cube is solved.
+ */
 
 int main(int argc, char* argv[]) try {
 	if (argc < 2) {
